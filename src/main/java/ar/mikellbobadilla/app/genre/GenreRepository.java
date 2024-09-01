14f08a688b27771893aa2890856e0bbeefdfd69a
sting <bobadillasting@gmail.com>
2024-08-31 23:00:33 -0300

    Feat(Genre): Add JPA Query methods GenreRepository

--- src/main/java/ar/mikellbobadilla/app/genre/GenreRepository.java
+++ src/main/java/ar/mikellbobadilla/app/genre/GenreRepository.java
@@ -3,8 +3,12 @@ package ar.mikellbobadilla.app.genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Set;
+
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
     boolean existsByNameIgnoreCase(String name);
     boolean existsByNameIgnoreCaseAndIdNot(String name, Long genreId);
+    Set<Genre> findAllByNameIgnoreCaseIn(List<String> names);
 }
