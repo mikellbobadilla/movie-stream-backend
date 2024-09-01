52f996e581a91a5742507dfc974aad43fcb2667f
sting <bobadillasting@gmail.com>
2024-08-31 23:00:02 -0300

    New(StorageUtils): Add StorageUtils.java tools

---
+++ src/main/java/ar/mikellbobadilla/app/utils/StorageUtils.java
@@ -0,0 +1,52 @@
+package ar.mikellbobadilla.app.utils;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
+
+public class StorageUtils {
+
+    public static void createDir(Path path) {
+        try {
+            if (Files.notExists(path)) Files.createDirectories(path);
+        } catch (IOException exc) {
+            throw new RuntimeException(exc);
+        }
+    }
+
+    public static void saveResource(Path posterPath, MultipartFile file) {
+        try {
+            Files.write(posterPath, file.getBytes(), CREATE_NEW);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void deleteResource(Path fullPosterPath) {
+        try {
+            Files.deleteIfExists(fullPosterPath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void forceDeleteDir(File file) {
+        if (file.isDirectory()) {
+            File[] children = file.listFiles();
+            if (children != null) {
+                for (File child: children) forceDeleteDir(child);
+            }
+            if (Objects.requireNonNull(file.listFiles()).length == 0) {
+                if (!file.delete()) System.err.println("Failed to delete dir" + file.getAbsolutePath());
+            }
+        } else {
+            if (!file.delete()) System.err.println("Failed to delete file " + file.getAbsolutePath());
+        }
+    }
+}
